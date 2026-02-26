df6f2bc704d910534d54022d05f274573e8bd864
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-02-26 16:09:05 +0700

    feat: Add app palette for colors

--- lib/presentation/dev/widget_showcase.dart
+++ lib/presentation/dev/widget_showcase.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:samudera/presentation/theme/app_palette.dart';
 
 class WidgetShowcaseScreen extends StatelessWidget {
   const WidgetShowcaseScreen({super.key});
@@ -7,7 +8,33 @@ class WidgetShowcaseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Dev: Widget Showcase")),
-      body: Column(children: []),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(width: double.infinity),
+            Text("AppPalette", textAlign: TextAlign.center),
+            Row(
+              spacing: 24,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: AppPalette.primaryGradient,
+                  ),
+                  width: 50,
+                  height: 50,
+                ),
+                Container(color: AppPalette.darkIndigo, width: 50, height: 50),
+                Container(color: AppPalette.lightIndigo, width: 50, height: 50),
+                Container(color: AppPalette.purple, width: 50, height: 50),
+                Container(color: AppPalette.vividBlue, width: 50, height: 50),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
