9b850f6a3f7c40751c420bd17a4ff2230b75ac5e
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-02-28 19:39:24 +0700

    feat: Implement GlobalButton widget for consistent button styles
    Added the GlobalButton widget with variants such as gradient, outlined, text, loading, and disabled

--- lib/presentation/dev/widget_showcase.dart
+++ lib/presentation/dev/widget_showcase.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:samudera/presentation/theme/app_palette.dart';
+import 'package:samudera/presentation/widgets/global/global_button.dart';
 
 class WidgetShowcaseScreen extends StatelessWidget {
   const WidgetShowcaseScreen({super.key});
@@ -8,34 +9,110 @@ class WidgetShowcaseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Dev: Widget Showcase")),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(width: double.infinity),
-            Text("AppPalette", textAlign: TextAlign.center),
-            SizedBox(height: 16),
-            Row(
-              spacing: 24,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: AppPalette.primaryGradient,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(width: double.infinity),
+              Text("AppPalette", textAlign: TextAlign.center),
+              SizedBox(height: 16),
+              Row(
+                spacing: 24,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: AppPalette.primaryGradient,
+                    ),
+                    width: 50,
+                    height: 50,
                   ),
-                  width: 50,
-                  height: 50,
-                ),
-                Container(color: AppPalette.darkIndigo, width: 50, height: 50),
-                Container(color: AppPalette.lightIndigo, width: 50, height: 50),
-                Container(color: AppPalette.purple, width: 50, height: 50),
-                Container(color: AppPalette.vividBlue, width: 50, height: 50),
-              ],
-            ),
-          ],
+                  Container(
+                    color: AppPalette.darkIndigo,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: AppPalette.lightIndigo,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(color: AppPalette.purple, width: 50, height: 50),
+                  Container(color: AppPalette.vividBlue, width: 50, height: 50),
+                ],
+              ),
+              _showcaseDivider(),
+              Text("Button", textAlign: TextAlign.center),
+              SizedBox(height: 16),
+              Column(
+                spacing: 12,
+                children: [
+                  GlobalButton(
+                    text: "Gradient Button",
+                    variant: ButtonVariant.gradient,
+                    onPressed: () {},
+                  ),
+                  GlobalButton(
+                    text: "Gradient Button w/ Icon",
+                    variant: ButtonVariant.gradient,
+                    icon: Icon(Icons.graphic_eq),
+                    onPressed: () {},
+                  ),
+                  GlobalButton(
+                    text: "Outlined Button",
+                    variant: ButtonVariant.outlined,
+                    onPressed: () {},
+                  ),
+                  GlobalButton(
+                    text: "Outlined Button w/ Icon",
+                    variant: ButtonVariant.outlined,
+                    icon: Icon(Icons.ac_unit),
+                    onPressed: () {},
+                  ),
+                  GlobalButton(
+                    text: "Text Button",
+                    variant: ButtonVariant.text,
+                    onPressed: () {},
+                  ),
+                  GlobalButton(
+                    text: "Text Button",
+                    onPressed: () {},
+                    isLoading: true,
+                  ),
+                ],
+              ),
+              _showcaseDivider(),
+              Text("Disabled Button", textAlign: TextAlign.center),
+              SizedBox(height: 16),
+              Column(
+                spacing: 12,
+                children: [
+                  GlobalButton(
+                    text: "Disabled Gradient Button",
+                    variant: ButtonVariant.gradient,
+                  ),
+                  GlobalButton(
+                    text: "Disabled Gradient Button",
+                    variant: ButtonVariant.outlined,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _showcaseDivider({double? height}) {
+    return Column(
+      children: [
+        SizedBox(height: height ?? 16),
+        Divider(),
+        SizedBox(height: height ?? 16),
+      ],
     );
   }
 }
